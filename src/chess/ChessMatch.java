4fc3d13eea3ccbfce61de6ebeaeb24842bb00603
Henrique Figueredo <henrique.lfigueredo@gmail.com>
2026-04-15 18:23:41 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,17 +32,20 @@ public class ChessMatch {
 		return (ChessPiece) capturedPiece;
 
 	}
-	
+
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
-		board.placePiece(p,  target);
+		board.placePiece(p, target);
 		return capturedPiece;
 	}
 
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no posible moves for the chosen piece");
 		}
 	}
 
